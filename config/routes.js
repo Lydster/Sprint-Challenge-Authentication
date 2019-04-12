fccba48b8eddc80720e5c7515c56864a7e0e9c2c
lydiathornton <lydia.d.thornton@gmail.com>
2019-04-12 09:33:36 -0700

    MVP - all routes working - going on to stretch

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require("axios");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const Users = require("./route-model");
+const secret = process.env.JWT_SECRET;
 const { authenticate } = require("../auth/authenticate");
 
 module.exports = server => {
@@ -24,7 +26,34 @@ function register(req, res) {
 }
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "invalid credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  return jwt.sign(payload, secret, options);
 }
 
 function getJokes(req, res) {
